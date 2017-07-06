26ad64aade293378639ea6f8388f9b7ee13a6a16
Matt Glover <matthewglover@me.com>
2017-07-06 14:41:35 +0100

    add values array and store item on push

--- src/test/java/Stack.java
+++ src/test/java/Stack.java
@@ -2,12 +2,14 @@ public class Stack {
 
     private int size;
     private int value;
+    private int[] values = new int[10];
 
     public int size() {
         return size;
     }
 
     public void push(int item) {
+        values[size] = item;
         value = item;
         size++;
     }
